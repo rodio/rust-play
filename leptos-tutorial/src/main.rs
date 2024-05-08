bc250ab55c3e3b099c81a00e9b3b32561dfc1b69
Rodion Borovyk <rodion.borovyk@gmail.com>
2024-05-08 22:26:41 +0200

    leptos dynamic classes

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -8,11 +8,15 @@ fn main() {
 fn App() -> impl IntoView {
     let (count, set_count) = create_signal(0);
     view! {
-        <button on:click=move |_| {
-            set_count.update(|n| *n += 1)
-        }>
+        <button
+            on:click=move |_| { set_count.update(|n| *n += 1) }
 
-            "Click me: " {count}
+            // the class: syntax reactively updates a single class
+            // here, we'll set the `red` class when `count` is odd
+            class:red=move || count() % 2 == 1
+        >
+            "Click me: "
+            {count}
         </button>
     }
 }
