8ed66bab65922e72013e5d3d5a82550ecf55f03f
Rodion Borovyk <rodion.borovyk@gmail.com>
2024-05-09 10:48:49 +0200

    dynamic styles and dynamic attributes

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -6,17 +6,33 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
+    let (x, set_x) = create_signal(0);
     let (count, set_count) = create_signal(0);
     view! {
         <button
-            on:click=move |_| { set_count.update(|n| *n += 1) }
+            on:click=move |_| {
+                set_x.update(|n| *n += 10);
+                set_count.update(|x| *x += 1);
+            }
 
-            // the class: syntax reactively updates a single class
-            // here, we'll set the `red` class when `count` is odd
-            class:red=move || count() % 2 == 1
+            // set the `style` attribute
+            style="position:absolute"
+            // and toggle individual CSS properties with `style`
+            style:left=move || format!("{}px", x() + 100)
+            style:background-color=move || format!("rgb({}, {}, 100)", x(), 100)
+            style:max-width="400px"
+            // Set a CSS variable for stylesheet use
+            style=("--columns", x)
         >
-            "Click me: "
-            {count}
+            "Click to Move"
         </button>
+        <br/>
+        <br/>
+        <progress
+            max="10"
+            // signals are functions, so `value=count` and `value=move || count.get()`
+            // are interchangeable.
+            value=count
+        ></progress>
     }
 }
