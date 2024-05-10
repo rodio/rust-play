6929992abf0b8e51c8bc83ee25f6b266c8f5cf9e
Rodion Borovyk <rodion.borovyk@gmail.com>
2024-05-10 19:35:41 +0200

    leptos: :woptional generic props

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -5,42 +5,21 @@ fn main() {
 }
 
 #[component]
-fn App() -> impl IntoView {
-    let (x, set_x) = create_signal(0);
+fn ProgressBar(#[prop(optional)] progress: Option<Box<dyn Fn() -> i32>>) -> impl IntoView {
+    progress.map(|progress| {
+        view! { <progress max=100 value=progress></progress> }
+    })
+}
+
+#[component]
+pub fn App() -> impl IntoView {
     let (count, set_count) = create_signal(0);
-    let double_count = move || count() * 2;
     view! {
-        <button
-            on:click=move |_| {
-                set_x.update(|n| *n += 10);
-                set_count.update(|x| *x += 1);
-            }
-
-            // set the `style` attribute
-            style="position:absolute"
-            // and toggle individual CSS properties with `style`
-            style:left=move || format!("{}px", x() + 100)
-            style:background-color=move || format!("rgb({}, {}, 100)", x(), 100)
-            style:max-width="400px"
-            // Set a CSS variable for stylesheet use
-            style=("--columns", x)
-        >
-            "Click to Move"
-        </button>
-
+        <button on:click=move |_| {
+            set_count.update(|n| *n += 1);
+        }>"Click me"</button>
         <br/>
         <br/>
-        <progress
-            max="10"
-            // signals are functions, so `value=count` and `value=move || count.get()`
-            // are interchangeable.
-            value=count
-        ></progress>
-
-        <br/>
-        <progress max="10" value=double_count></progress>
-
-        <br/>
-        <p>"Double Count: " {double_count}</p>
+        <ProgressBar progress=Box::new(count)/>
     }
 }
