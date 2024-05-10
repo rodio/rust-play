2626f8d47c3d4c25ebd736c39e80ab39752e4f1c
Rodion Borovyk <rodion.borovyk@gmail.com>
2024-05-10 21:24:24 +0200

    leptos dynamic list with <For/>

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -6,20 +6,59 @@ fn main() {
 
 #[component]
 pub fn App() -> impl IntoView {
-    let length = 5;
-    let counters = (1..=length).map(|idx| create_signal(idx));
+    view! {
+        <DynamicList initial_length=2/>
+    }
+}
 
-    // each item manages a reactive view
-    // but the list itself will never change
-    let counter_buttons = counters
-        .map(|(count, set_count)| {
-            view! {
-                <li>
-                    <button on:click=move |_| set_count.update(|n| *n += 1)>{count}</button>
-                </li>
-            }
-        })
-        .collect_view();
+#[component]
+fn DynamicList(initial_length: usize) -> impl IntoView {
+    let mut next_counter_id = initial_length;
+    let initial_counters = (0..initial_length)
+        .map(|id| (id, create_signal(id + 1)))
+        .collect::<Vec<_>>();
 
-    view! { <ul>{counter_buttons}</ul> }
+    // now we store that initial list in a signal
+    // this way, we'll be able to modify the list over time,
+    // adding and removing counters, and it will change reactively
+    let (counters, set_counters) = create_signal(initial_counters);
+    let add_counter = move |_| {
+        let sig = create_signal(next_counter_id + 1);
+        set_counters.update(move |counters| counters.push((next_counter_id, sig)));
+        next_counter_id += 1;
+    };
+
+    view! {
+        <div>
+            <button on:click=add_counter>"Add counter"</button>
+            <ul>
+                <For
+                    each=counters
+                    key=|counter| counter.0
+                    children=move |(id, (count, set_count))| {
+                        view! {
+                            <li>
+                                <button on:click = move |_| set_count.update(| n | * n += 1)>
+                                    { count }
+                                </button>
+                                <button on:click = move|_| {
+                                    set_counters
+                                        .update(|counters| {
+                                            counters.retain(|(counter_id, (signal, _))| {
+                                                if counter_id == &id {
+                                                    signal.dispose();
+                                                }
+                                                counter_id != &id
+
+                                            })
+                                        })
+                                } >
+                            "Remove" </ button > </ li >
+                        }
+                    }
+                />
+
+            </ul>
+        </div>
+    }
 }
