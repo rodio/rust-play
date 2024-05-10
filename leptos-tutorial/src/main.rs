2e7c4fbb1495c758c3a8ec7903b44ed6d1e60eb1
Rodion Borovyk <rodion.borovyk@gmail.com>
2024-05-10 20:33:24 +0200

    leptos iterators2

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -1,4 +1,4 @@
-use leptos::*;
+use leptos::{svg::view, *};
 
 fn main() {
     leptos::mount_to_body(App);
@@ -6,11 +6,20 @@ fn main() {
 
 #[component]
 pub fn App() -> impl IntoView {
-    let values = vec![0, 1, 2];
+    let length = 5;
+    let counters = (1..=length).map(|idx| create_signal(idx));
 
-    view! {
-        <p>{values.clone()}</p>
-        // or we can wrap them in li
-        <ul>{values.into_iter().map(|n| view! { <li>{n}</li> }).collect::<Vec<_>>()}</ul>
-    }
+    // each item manages a reactive view
+    // but the list itself will never change
+    let counter_buttons = counters
+        .map(|(count, set_count)| {
+            view! {
+                <li>
+                    <button on:click=move |_| set_count.update(|n| *n += 1)>{count}</button>
+                </li>
+            }
+        })
+        .collect_view();
+
+    view! { <ul>{counter_buttons}</ul> }
 }
