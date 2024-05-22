1b187fbbc4c54f0c4dbfec053882c1ef59e2e0a0
Rodion Borovyk <rodion.borovyk@gmail.com>
2024-05-22 15:36:47 +0200

    leptos: controlled input

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -4,54 +4,16 @@ fn main() {
     leptos::mount_to_body(App);
 }
 
-#[derive(Debug, Clone)]
-struct DatabaseEntry {
-    key: String,
-    value: i32,
-}
-
 #[component]
 pub fn App() -> impl IntoView {
-    // start with a set of three rows
-    let (data, set_data) = create_signal(vec![
-        DatabaseEntry {
-            key: "foo".to_string(),
-            value: 10,
-        },
-        DatabaseEntry {
-            key: "bar".to_string(),
-            value: 20,
-        },
-        DatabaseEntry {
-            key: "baz".to_string(),
-            value: 15,
-        },
-    ]);
+    let (name, set_name) = create_signal("Controlled".to_string());
+
     view! {
-        // when we click, update each row,
-        // doubling its value
-        <button on:click=move |_| {
-            set_data.update(|data| {
-                for row in data {
-                    row.value *= 2;
-                }
-            });
-            // log the new value of the signal
-            logging::log!("{:?}", data.get());
-        }>
-            "Update Values"
-        </button>
-        <For
-            each=move || data().into_iter().enumerate()
-            key=|(_, state)| state.key.clone()
-            children=move |(index, _)| {
-                let value = create_memo(move |_| {
-                    data.with(|data| data.get(index).map(|d| d.value).unwrap_or(0))
-                });
-                view! {
-                    <p>{value}</p>
-                }
-            }
-        />       // iterate over the rows and display each value
+        <input
+            type="text"
+            on:input=move |ev| { set_name(event_target_value(&ev)) }
+            prop:value=name
+        />
+        <p>"Name is " {name}</p>
     }
 }
