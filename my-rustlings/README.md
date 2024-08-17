f1747d9984324542147bf8fe4c80c9a3525b1e60
Rodion Borovyk <rodion.borovyk@gmail.com>
2024-08-17 17:52:59 +0200

    add rustlings

---
+++ my-rustlings/README.md
@@ -0,0 +1,3 @@
+# Answers to Rustlings
+
+This is my take on Rustlings https://github.com/rust-lang/rustlings
---
+++ my-rustlings/exercises/options/options2.rs
@@ -0,0 +1,41 @@
+// options2.rs
+//
+// Execute `rustlings hint options2` or use the `hint` watch subcommand for a
+// hint.
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn simple_option() {
+        let target = "rustlings";
+        let optional_target = Some(target);
+
+        if let Some(word) = optional_target {
+            assert_eq!(word, target);
+        }
+    }
+
+    #[test]
+    fn layered_option() {
+        let range = 10;
+        let mut optional_integers: Vec<Option<i8>> = vec![None];
+
+        for i in 1..(range + 1) {
+            optional_integers.push(Some(i));
+        }
+
+        let mut cursor = range;
+
+        // TODO: make this a while let statement - remember that vector.pop also
+        // adds another layer of Option<T>. You can stack `Option<T>`s into
+        // while let and if let.
+        while let Some(integer_opt) = optional_integers.pop() {
+            if let Some(integer) = integer_opt {
+                assert_eq!(integer, cursor);
+                cursor -= 1;
+            }
+        }
+
+        assert_eq!(cursor, 0);
+    }
+}
